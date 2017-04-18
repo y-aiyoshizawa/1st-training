8254432d8fa9d7c8f8ca6501a9880b4506dbcc3e
yuta <yuta@yuta-PC>
2017-04-19 08:14:45 +0900

    ソートができるように

--- Lesson7/CharacterCount.java
+++ Lesson7/CharacterCount.java
@@ -1,9 +1,25 @@
-import java.util.*;
-class CharacterCount{
-    public static void main(String[] args){
-        Map<Character,Integer> map = new TreeMap<Character,Integer>();
 
-        Scanner scan= new Scanner(System.in);
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+class CharacterCount{
+
+    public static void main(String[] args){
+
+        Map<Character,Integer> map = new TreeMap<Character,Integer>();
+        Scanner scan = null;
+        try {
+            scan = new Scanner(Paths.get("E:\\user\\yuta\\Documents\\Git\\1st-training\\Lesson10\\Sample7.java"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         //処理
         while(scan.hasNext()){
@@ -22,27 +38,34 @@ class CharacterCount{
             }
         }
 
-        //最大値を取得
-        int max = 0;
-        for(char key: map.keySet()){
-            if(map.get(key) > max){
-                max = map.get(key);
-            }
+
+        ArrayList<Entry<Character,Integer>> list = new ArrayList<>();
+
+        for(Entry<Character,Integer> e : map.entrySet()){
+            list.add(e);
         }
+        Collections.sort(list, (a,b) ->b.getValue() - a.getValue());
+
+        for(Entry<Character,Integer> e : list){
+            System.out.println(e.getKey() + "=" + e.getValue());
+        }
+
+        //最大値を取得
+        int max = list.get(0).getValue();
         //*を表示
-        for(int i = max; i >= 0; i--){
-            for(char key: map.keySet()){
-                if(map.get(key) >= i){
+        for(int i = max; i > 0; i--){
+            for(Entry<Character,Integer> e : list){
+                if(e.getValue() > i){
                     System.out.print("*");
                 }else{
-                    System.out.print(" ");
+                    break;
                 }
             }
             System.out.println();
         }
         //文字を表示
-        for(char key: map.keySet()){
-            System.out.print(key);
+        for(Entry<Character,Integer> e : list){
+            System.out.print(e.getKey());
         }
     }
 }
