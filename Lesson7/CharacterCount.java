2584a8dd2f519bd68100698e6498cc4f2a3f69a6
TTCAD\y-aiyoshizawa <y-aiyoshizawa@ttc-net.co.jp>
2017-04-18 18:05:28 +0900

    表示の仕方を変更

--- Lesson7/CharacterCount.java
+++ Lesson7/CharacterCount.java
@@ -1,22 +1,48 @@
+import java.util.*;
 class CharacterCount{
-    public static void main(String[] args) throws IOException{
-        Map<Character,Integer> map = new HashMap<Character,Integer>();
+    public static void main(String[] args){
+        Map<Character,Integer> map = new TreeMap<Character,Integer>();
 
         Scanner scan= new Scanner(System.in);
 
+        //処理
         while(scan.hasNext()){
             String str = scan.next();
             for(int i = 0; i < str.length(); i++){
                 char c = str.charAt(i);
-                if(map.containsKey(c)){
-                    map.put(c,map.get(c) + 1);
-                }else{
-                    map.put(c,1);
+                if(    ('0' <= c && c <= '9')
+                    || ('A' <= c && c <= 'Z')
+                    || ('a' <= c && c <= 'z')){
+                    if(map.containsKey(c)){
+                        map.put(c,map.get(c) + 1);
+                    }else{
+                        map.put(c,1);
+                    }
                 }
             }
         }
+
+        //最大値を取得
+        int max = 0;
         for(char key: map.keySet()){
-            System.out.println(key + "=" + map.get(key));
+            if(map.get(key) > max){
+                max = map.get(key);
+            }
+        }
+        //*を表示
+        for(int i = max; i >= 0; i--){
+            for(char key: map.keySet()){
+                if(map.get(key) >= i){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        //文字を表示
+        for(char key: map.keySet()){
+            System.out.print(key);
         }
     }
 }
