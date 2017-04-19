1e1e4aa8f80114a2434bda9ea4de0e91e6d7202b
TTCAD\y-aiyoshizawa <y-aiyoshizawa@ttc-net.co.jp>
2017-04-19 18:31:25 +0900

    円周率を求めるプログラム

---
+++ test/En.java
@@ -0,0 +1,30 @@
+class En{
+    public static void main(String[] args) {
+        a(10000);
+    }
+    public static void random(int max){
+        int c = 0;
+        for(int i = 0; i < max; i++){
+            double x = Math.random();
+            double y = Math.random();
+            if(x * x + y * y < 1){
+                c++;
+            }
+        }
+        System.out.println((double)c / max * 4);
+    }
+    public static void a(int max){
+        int c = 0;
+        for(int i = 0; i < max; i++){
+            for(int j = 0; j < max; j++){
+                double x = (double)i / max;
+                double y = (double)j / max;
+                if(x * x + y * y < 1){
+                    c++;
+                }
+            }
+        }
+        System.out.println((double)c / max * max * 4);
+    }
+
+}
