d84262afc68d3492ee7bbc83472c4dcdf76eecb1
TTCAD\y-aiyoshizawa <y-aiyoshizawa@ttc-net.co.jp>
2017-04-21 17:21:56 +0900

    Lesson14

--- Lesson14/Sample10.java
+++ Lesson14/Sample10.java
@@ -4,16 +4,27 @@ import java.io.*;
 
 public class Sample10 {
     public static void main(String[] args) {
+        if(args.length != 1){
+            System.out.println("引数の数が違います");
+            System.exit(1);
+        }
+
+        BufferedReader br = null;
         try{
-            BufferedReader br = new BufferedReader(new FileReader(args[0]));
+            br = new BufferedReader(new FileReader(args[0]));
             String str = "";
             while((str = br.readLine()) != null){
                 System.out.println(str);
             }
-            br.close();
 
         }catch(IOException e){
             System.out.println("入出力エラーです");
+        }finally{
+            try{
+                br.close();
+            }catch(IOException e){
+                
+            }
         }
     }
 }
--- Lesson14/Sample7.java
+++ Lesson14/Sample7.java
@@ -1,18 +1,20 @@
 import java.io.*;
+import java.util.*;
 
 public class Sample7 {
     public static void main(String[] args) {
 
         try{
+            Scanner scan = new Scanner(System.in,"utf-8");
             PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("test.txt")));
-            
-            pw.println("hello");
+
+            pw.println(scan.next());
             pw.print("world");
             System.out.println("ファイルに書き込みました");
             pw.close();
-            
+
         }catch(IOException e){
-            System.out.println("入出力エラーです");
+            System.out.println("入出力エラーです"+e);
         }
     }
 }
